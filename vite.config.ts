011d24a39c1f665a92547d1c9fb0c6c0e80ea0d4
Masamune Ishihara <1396267+masaishi@users.noreply.github.com>
2024-05-03 02:22:06 -0700

    Write ch5 2 (#2)
    * Add switcher
    
    * Delete scripts
    
    * Write ch5.2
    
    * Downgrade npm to 18

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig(({ command, mode }) => {
+	if (command == 'serve') {
+		return {
+			base: '/',
+		}
+	}
   return {
     plugins: [],
 		// TODO: Uncomment or unmerged to main.
